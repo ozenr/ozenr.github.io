65bf4885d1d834cbd70300847693de44128566e9
ozenr <ouenramb@gmail.com>
2026-03-09 13:24:19 -0400

    added animations for cards sliding in/out

--- src/cards.js
+++ src/cards.js
@@ -1,18 +1,41 @@
 import './style.css'
 
-const button = document.querySelector('.swipe');
+const rightBtn = document.querySelector('.swipe-right');
+const leftBtn = document.querySelector('.swipe-left');
 const cards = document.querySelectorAll('.cards img');
 
 // Button Press
 let i = 0;
-button.addEventListener('click', () => {
-    console.log(i);
-    console.log(cards.length);
+cards[0].classList.add('slide-in');
+
+rightBtn.addEventListener('click', () => {
+    // Remove Current Card
+    cards[i].classList.remove('slide-in', 'slide-out');
     cards[i].style.display = 'none';
+
     i++;
     if (i >= cards.length) {
-        button.style.display = 'none';
+        rightBtn.style.display = 'none';
+        leftBtn.style.display = 'none';
         return;
     }
+
+    // Add Current Card
+    cards[i].classList.add('slide-in');
+    cards[i].style.display = 'block';
+})
+
+leftBtn.addEventListener('click', () => {
+    if (i <= 0) {
+        return;
+    }
+
+    // Remove Current Card
+    cards[i].classList.remove('slide-in', 'slide-out');
+    cards[i].style.display = 'none';
+
+    i--;
+    // Go Back to Previous Card
+    cards[i].classList.add('slide-out');
     cards[i].style.display = 'block';
 })
